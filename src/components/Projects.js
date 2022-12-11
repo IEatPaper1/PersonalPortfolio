4d75fffc307ccecd65fee498ee9308f921615aa1
unknown <Mr.MiniExplosion@gmail.com>
2022-12-11 11:41:41 -0500

    small change

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { AiFillGithub } from "react-icons/ai";
 const Projects = () => {
   return (
     <div class="flex justify-center gap-20 mt-12 text-center">
@@ -14,6 +14,11 @@ const Projects = () => {
         <div class="p-5">
           <div class="underline font-bold">Personal Portfolio</div>Site where I
           put stuff I did so far using react and tailwind.css
+        </div>
+        <div class="ml-20">
+          <a href="https://github.com/IEatPaper1/PersonalPortfolio">
+            <AiFillGithub size={50} />
+          </a>
         </div>
       </div>
       <div class="bg-emerald-400 w-[200px] h-[200px]"></div>
